35f67152d763f33502cdc3e4f0c0c1a53c9c184f
amirhoseinaghaei <amirhoseinaghaeii@yahoo.com>
2020-12-15 01:02:12 +0330

    h files added

---
+++ h/bfs.h
@@ -0,0 +1,22 @@
+#if !defined(BFS_H)
+#define BFS_H
+
+#define N 3
+#include <iostream>
+#include <vector>
+class bfs
+{
+public:
+    bfs(); 
+    bfs(bfs *pr, int mat[N][N]);
+    bfs(int mat[N][N]);
+    
+    // stores the parent node of the current node
+    // helps in tracing path when the answer is found
+    bfs *parent;
+    int matrix[N][N];
+    std::vector<bfs> childs ; 
+    // std::vector<int[N][N]>
+};
+
+#endif
---
+++ h/project.h
@@ -0,0 +1,13 @@
+#if !defined(PROJECT_H)
+#define PROJECT_H
+#include <iostream>
+#define N 3 
+#include "bfs.h"
+
+void disp_matrix(int matrix[N][N]) ;
+void solve_bfs(bfs a , int gm[N][N] ,int m[N][N] ) ; 
+
+#endif // PROJECT_H
+
+
+
